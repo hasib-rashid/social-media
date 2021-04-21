f29661c20bf9b6432263dcd4280b11703fa47f51
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-21 14:05:36 +0600

    🔨Testing CSS

---
+++ pages/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <main className={styles.Header}>
+            <h1>Header</h1>
+        </main>
+    )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import Head from "next/head";
 import "../styles/Home.module.css";
+import Header from "./components/Header/Header";
 
 const index = () => {
     return (
         <div>
             <Head>
-                <title>Custom Title</title>     
+                <title>Custom Title</title>
             </Head>
 
-            <h1>Hello NextJS</h1>
+            <Header />
         </div>
     )
 }
