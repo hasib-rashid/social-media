273db871542b3f64c3b605d43580de64fc98bb62
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-21 13:51:53 +0600

    🔨Impleamenting and Fixing Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Head from "next/head";
+import "../styles/Home.module.css";
+
+const index = () => {
+    return (
+        <div>
+            <Head>
+                <title>Custom Title</title>     
+            </Head>
+
+            <h1>Hello NextJS</h1>
+        </div>
+    )
+}
+
+export default index
